43288057675451f46ad2f1f6de35a2898c260d41
MAC-Anas <apple@MacBook-Air-de-MAC.local>
2018-06-24 12:20:42 +0100

    Ajout des controlleurs principaux

---
+++ MonRestoDuMonde/Controlleur/MenuController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by MAC-Anas on 24/06/2018.
+//  Copyright © 2018 MAC-Anas. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
