65405236166e3874c1a17ea14f66d29151eafdb1
MAC-Anas <apple@MacBook-Air-de-MAC.local>
2018-06-24 12:46:55 +0100

    TabBarCustom

---
+++ MonRestoDuMonde/Controlleur/MonTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by MAC-Anas on 24/06/2018.
+//  Copyright © 2018 MAC-Anas. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+
+}
---
+++ MonRestoDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,14 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by MAC-Anas on 24/06/2018.
+//  Copyright © 2018 MAC-Anas. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
+
