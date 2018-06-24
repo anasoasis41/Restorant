0c4df8d4818beafdb896ccc2795910f6738ab212
MAC-Anas <apple@MacBook-Air-de-MAC.local>
2018-06-24 14:02:39 +0100

    Class Plat

---
+++ MonRestoDuMonde/Modele/Enums.swift
@@ -0,0 +1,16 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by MAC-Anas on 24/06/2018.
+//  Copyright © 2018 MAC-Anas. All rights reserved.
+//
+
+import Foundation
+
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+}
---
+++ MonRestoDuMonde/Modele/Plat.swift
@@ -0,0 +1,42 @@
+//
+//  Plat.swift
+//  MonRestoDuMonde
+//
+//  Created by MAC-Anas on 24/06/2018.
+//  Copyright © 2018 MAC-Anas. All rights reserved.
+//
+
+import UIKit
+
+
+class Plat {
+    
+    private var _type: Type
+    private var _pays: String
+    private var _desc: String
+    private var _image: UIImage
+    
+    var type: Type {
+        return _type
+    }
+    
+    var pays: String {
+        return _pays
+    }
+    
+    var desc: String {
+        return _desc
+    }
+    
+    var image: UIImage {
+        return _image
+    }
+    
+    init(type: Type, pays: String, desc: String, image: UIImage) {
+        self._type = type
+        self._pays = pays
+        self._desc = desc
+        self._image = image
+    }
+    
+}
