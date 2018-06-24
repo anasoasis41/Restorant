50019b40159961ff165a6f3ec312b83f0908c863
MAC-Anas <apple@MacBook-Air-de-MAC.local>
2018-06-24 13:46:00 +0100

    Ajout Collection View

---
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by MAC-Anas on 24/06/2018.
+//  Copyright © 2018 MAC-Anas. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
