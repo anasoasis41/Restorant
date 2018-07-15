91ac38d8aa54bffd7d5d18547efac70038452d31
MAC-Anas <apple@MacBook-Air-de-MAC.local>
2018-07-15 14:44:41 +0100

    creation de ui plat de facon programmatique

--- MonRestoDuMonde/Controlleur/DetailController.swift
+++ MonRestoDuMonde/Controlleur/DetailController.swift
@@ -26,7 +26,30 @@ class DetailController: UIViewController {
     }
     
     func miseEnPlace(menu: Menu) {
-        print("Ca marche on a le menu: \(menu.pays)")
+        let imageDuMenu = UIImageView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 250))
+        imageDuMenu.image = menu.image
+        imageDuMenu.contentMode = .scaleAspectFill
+        imageDuMenu.clipsToBounds = true
+        scrolll.addSubview(imageDuMenu)
+        var maxY = imageDuMenu.frame.maxY
+        
+        let nomDuMenu = UILabel(frame: CGRect(x: 0, y: maxY, width: view.frame.width, height: 100))
+        nomDuMenu.text = "Menu " + menu.pays
+        nomDuMenu.font = FONT_TITRE
+        nomDuMenu.textColor = .red
+        nomDuMenu.adjustsFontSizeToFitWidth = true
+        nomDuMenu.textAlignment = .center
+        scrolll.addSubview(nomDuMenu)
+        maxY = nomDuMenu.frame.maxY
+        
+        for plat in menu.plats {
+            let vue = PlatVue(frame: CGRect(x: 0, y: maxY, width: view.frame.width, height: 375))
+            vue.miseEnPlace(plat: plat)
+            scrolll.addSubview(vue)
+            maxY = vue.frame.maxY
+        }
+        
+        scrolll.contentSize = CGSize(width: view.frame.width, height: maxY + 50)
     }
 
 }
